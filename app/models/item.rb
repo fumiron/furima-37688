ccdaf85c44375514c1757deba85499e5f3a59ddb
fumiron <haa13022@nuhw.ac.jp>
2022-04-07 16:05:42 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,12 @@ class Item < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :delivery_time
+
 end
