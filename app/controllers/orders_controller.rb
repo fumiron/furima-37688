aa736b90fa86104ffce874ddd33c7bf34202cb82
fumiron <haa13022@nuhw.ac.jp>
2022-04-14 11:18:40 +0900

    コントローラーの重複箇所のリファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,13 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new
     redirect_to root_path if @item.user_id == current_user.id || @item.order
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new(order_delivery_params)
     if @order_delivery.valid?
       pay_item
@@ -34,5 +33,9 @@ class OrdersController < ApplicationController
       card: order_delivery_params[:token],
       currency: 'jpy'
     )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
